6b68e3c498678aed7aa31d3286d278ccaaf4e320
Fylyheng <fylyheng.phally@gmail.com>
2021-07-20 11:29:21 +0700

    add data class Account.kt

--- src/main/kotlin/com/example/springBoot25/model/Account.kt
+++ src/main/kotlin/com/example/springBoot25/model/Account.kt
@@ -1,6 +1,13 @@
 package com.example.springBoot25.model
 
+import javax.persistence.*
+
+@Entity
 data class Account(
+        @Id
+        @SequenceGenerator(name = "account_seq", sequenceName = "account_seq",allocationSize = 1)
+        @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "account_seq")
         var id:Long,
+
         var name :String
 )
